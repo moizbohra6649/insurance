2c967de57d28d8582688f2e40e748e7a5ca914d7
Moiz Bohra <tandamoiz@gmail.com>
2025-04-21 01:55:14 +0530

    create a new dashboard php file for title show for now.

---
+++ php/dashboard_php.php
@@ -0,0 +1,18 @@
+<?php
+/* Include Function's File */
+if (file_exists(dirname(__DIR__) . '/partial/functions.php')) {
+    require_once(dirname(__DIR__) . '/partial/functions.php');
+}
+
+$title      = "Dashboard"; 
+$breadcrumb_title = "Dashboard";
+$local_mode = "";
+$readonly   = "";
+$id         = (isset($_REQUEST["id"]) && !empty($_REQUEST["id"])) ? base64_decode($_REQUEST["id"]) : 0;
+$mode       = (isset($_REQUEST["mode"])) ? $_REQUEST["mode"] : "NEW";
+$form_request = (isset($_REQUEST["form_request"])) ? $_REQUEST["form_request"] : "false";
+$error_msg  = (isset($_REQUEST["error_msg"])) ? $_REQUEST["error_msg"] : "";
+
+
+
+?>
