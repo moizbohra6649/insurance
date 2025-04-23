0ca71402bcf5f22b99b07ffc88351700cb3af2af
Moiz Bohra <tandamoiz@gmail.com>
2025-04-24 00:17:05 +0530

    Logo changes and other csss updation in assets folder.

--- admin/partial/topbar.php
+++ admin/partial/topbar.php
@@ -12,7 +12,7 @@
             </div>
         </form>
         <div class="header-logo-wrapper col-auto p-0">
-            <div class="logo-wrapper"><a href="index.html"><img class="img-fluid" src="assets/images/logo/logo.png" alt=""></a></div>
+            <div class="logo-wrapper"><a href="index.php"><img class="img-fluid" src="assets/images/logo/logo.svg" alt=""></a></div>
             <div class="toggle-sidebar"><i class="status_toggle middle sidebar-toggle" data-feather="align-center"></i></div>
         </div>
         
