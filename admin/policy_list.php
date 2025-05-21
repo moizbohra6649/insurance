6fc5f6457c28855748f11e4a3f778395846237d4
Shubham Patidar <patidarshubham12345@gmail.com>
2025-05-21 22:36:55 +0530

    add card desing

--- admin/policy_list.php
+++ admin/policy_list.php
@@ -64,6 +64,9 @@ include('partial/loader.php'); ?>
                                                     <td align="center"> <?=convert_db_date_readable($get_data["created"])?> </td>
                                                     
                                                     <td align="center">
+                                                         <a href="<?=$actual_link?>reports/card.html?id=<?=base64_encode($id)?>" target="_blank" class="action-icon m-2">
+                                                    <i class="fa fa-file-pdf-o" aria-hidden="true"></i>
+                                                    </a>
                                                     <a href="<?=$actual_link?>reports/policy.php?id=<?=base64_encode($id)?>" target="_blank" class="action-icon m-2">
                                                     <i class="fa fa-file-pdf-o" aria-hidden="true"></i>
                                                     </a>
